96570f6fad26c001e4f0d44d0e51f849d4e02389
brendaConte <brendamconte@gmail.com>
2023-05-31 17:26:55 -0300

    sign up

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+//import { useState } from "react";
 import "./App.css";
+import {Route , Routes} from "react-router-dom" ;
+import SignUp from "./components/SignUp" ;
+
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p>hola!</p>
-    </>
+<Routes>
+<Route path="/signup" element={ <SignUp/> } />
+</Routes>
+   
   );
 }
 
