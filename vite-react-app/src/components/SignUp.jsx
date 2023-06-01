4808ddf4f99b30e62473b5e5d63af54950bad3ed
jorgefortaleche <jorgefh08@hotmail.com>
2023-06-01 19:01:11 -0300

    login front done

--- vite-react-app/src/components/SignUp.jsx
+++ vite-react-app/src/components/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import useInput from "../hooks/useInput";
 import axios from "axios";
-import { axiosURL } from "../settings";
+import { axiosURL } from "../settings/url";
 import { useNavigate } from "react-router";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -29,75 +29,45 @@ export default function SignUp() {
         },
         { withCredentials: true }
       )
-      .then((res) => navigate("/")) // pendiente:crear ruta p/el navigate
+      .then((res) => navigate("/"))
       .catch((error) => console.log(error));
   };
 
   return (
-    <div className="container-fluid p-3" style={{ background: "#2B2D42" }}>
-      <Card
-        className="container-fluid p-3 card-form"
-        style={{ width: "50rem" }}
-      >
-        <Form className="container" onSubmit={handleSubmit}>
-          <div className="row">
-            <Form.Group className="mb-3 p-2 col-6" controlId="formBasicEmail">
-              <div className="container">
-                <div className="row">
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    required
-                    {...name}
-                    type="text"
-                    placeholder="name"
-                  />
-                </div>
-              </div>
-            </Form.Group>
-            <Form.Group className="mb-3 p-2" controlId="formBasicEmail">
-              <Form.Label>lastname</Form.Label>
-              <Form.Control
-                required
-                {...lastName}
-                type="text"
-                placeholder="lastName"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3 p-2" controlId="formBasicEmail">
-              <Form.Label>email</Form.Label>
-              <Form.Control
-                required
-                {...email}
-                type="text"
-                placeholder="email"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3 p-2 col-8" controlId="formBasicEmail">
-              <Form.Label>password</Form.Label>
-              <Form.Control
-                required
-                {...password}
-                type="text"
-                placeholder="direction"
-              />
-            </Form.Group>
-            <Form.Group className="mb-3 p-2 col-4" controlId="formBasicEmail">
-              <Form.Label>phone number</Form.Label>
-              <Form.Control
-                required
-                {...phone}
-                type="number"
-                placeholder="code"
-              />
-            </Form.Group>
-            <div className="col-12">
-              <Button variant="secondary" size="lg" type="submit">
-                send
-              </Button>
-            </div>
-          </div>
-        </Form>
-      </Card>
-    </div>
+    <Card style={{ width: "20rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control required {...name} type="text" placeholder="Nombre" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control
+            required
+            {...lastName}
+            type="text"
+            placeholder="Apellido"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control required {...email} type="text" placeholder="Email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control
+            required
+            {...password}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Control required {...phone} type="text" placeholder="Celular" />
+        </Form.Group>
+        <div className="col-12">
+          <Button variant="outline-info" size="lg" type="submit">
+            Sing up
+          </Button>
+        </div>
+      </Form>
+    </Card>
   );
 }
