6192ef7b16094aa0896c8e910e92f251e6431c56
jorgefortaleche <jorgefh08@hotmail.com>
2023-06-14 10:29:55 -0300

    filter-categories

--- vite-react-app/src/state/properties.js
+++ vite-react-app/src/state/properties.js
@@ -2,7 +2,7 @@ import { createReducer, createAction } from "@reduxjs/toolkit";
 
 export const getState = createAction("GET_STATE");
 
-const initialState = "alquiler";
+const initialState = "alquiler"; // con el handleClick hago que cambie mi inicialState("alquiler"), pasa a ser "venta".
 
 const stateReducer = createReducer(initialState, {
   [getState]: (state, action) => action.payload,
