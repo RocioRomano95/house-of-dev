8d3e6b165decf1fbb418632d8162489e5c6c0837
brendaConte <brendamconte@gmail.com>
2023-06-14 15:26:11 -0300

    structure edit user

--- vite-react-app/src/components/User/EditUser.jsx
+++ vite-react-app/src/components/User/EditUser.jsx
@@ -4,13 +4,33 @@ import NavBar from '../Navbar/NavBar';
 import { axiosURL } from "../../settings/url";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { TabContainer } from 'react-bootstrap';
 
 
 export default function EditUser() {
 
 
-
-  return (
-    <div>EditUser</div>
+  return (  <>
+  <Container fluid>
+    <Form.Floating className="mb-3">
+      <Form.Control
+        id="floatingInputCustom"
+        type="email"
+        placeholder="name@example.com"
+      />
+      <label htmlFor="floatingInputCustom">Email address</label>
+    </Form.Floating>
+    <Form.Floating>
+      <Form.Control
+        id="floatingPasswordCustom"
+        type="password"
+        placeholder="Password"
+      />
+      <label htmlFor="floatingPasswordCustom">Password</label>
+    </Form.Floating>
+    </Container>
+  </>
   )
 }
+{/* <div>EditUser</div> */}
+ 
